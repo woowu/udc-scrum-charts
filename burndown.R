fe52c3b956c9175c10b339f94753a9b025722bdc
Woody Wu <woody.wu2@honeywell.com>
2019-08-25 13:33:01 +0800

    theme

--- burndown.R
+++ burndown.R
@@ -7,10 +7,9 @@ curr_sprint <- 11
 annotation_sz <- 3
 
 my_theme <- function() {
-    theme_bw(base_size=12, base_family = 'SF Pro Display') +
+    theme_minimal(base_size=12, base_family = 'SF Pro Display') +
         theme(legend.title = element_blank()) +
-        theme(plot.title = element_text(vjust = 1.25, face = 'bold')) +
-        theme(plot.margin = unit(c(1, .5, .75, 1), "cm"))
+        theme(plot.title = element_text(vjust = 1.25, face = 'bold'))
 }
 
 dat <- read.csv('burndown.csv')
@@ -32,27 +31,29 @@ plot <- ggplot(burndown, aes(sprint, efforts)) +
     geom_line(aes(color = type, size = type != 'baseline')) +
     scale_x_continuous(limits = c(min_sprint, max_sprint),
         breaks = seq(min_sprint, max_sprint, by=1), name = 'sprint (2w)') +
-    scale_y_continuous(limits = c(0, max_efforts),
-        breaks = seq(0, max_efforts, by=ystep), name = 'remaining effort (points)') +
+    scale_y_continuous(limits = c(0, max_efforts)
+        , breaks = seq(0, max_efforts, by=ystep)
+        , name = 'remaining effort (points)') +
     scale_size_manual(values = c(0.5, 1.2), guide = FALSE) +
-    geom_segment(aes(x = curr_sprint, xend = curr_sprint, y = 0, yend = 554.40 - 10),
-                 color = guide_line_color, size = guide_line_sz) +
+    geom_segment(aes(x = curr_sprint, xend = curr_sprint
+                     , y = 0, yend = 554.40 - 10)
+                    , color = guide_line_color, size = guide_line_sz) +
     geom_segment(aes(x = 17, xend = 17, y = 0, yend = 373.2 - 10),
                  color = guide_line_color, size = guide_line_sz) +
     geom_segment(aes(x = 21, xend = 21, y = 0, yend = 252.4 - 10),
                  color = guide_line_color, size = guide_line_sz) +
     geom_segment(aes(x = 26, xend = 26, y = 0, yend = 101.4 - 10),
                  color = guide_line_color, size = guide_line_sz) +
-    annotate('text', x = curr_sprint, y = 554.40 + 10, hjust = 0,
-             size = annotation_sz, label = 'Today') +
-    annotate('text', x = 17, y = 373.2 + 10, hjust = 0,
-             size = annotation_sz, label = 'Arch. Matured') +
-    annotate('text', x = 21, y = 252.4 + 10, hjust = 0,
-             size = annotation_sz, label = 'Full Functional') +
-    annotate('text', x = 26, y = 101.4 + 10, hjust = 0,
-             size = annotation_sz, label = 'Feature Rich') +
-    labs(title = 'Efforts Burndown',
-         subtitle = 'Planned vs Actual') +
+    annotate('text', x = curr_sprint, y = 554.40 + 10, hjust = 0
+             , size = annotation_sz, label = 'Today') +
+    annotate('text', x = 17, y = 373.2 + 10, hjust = 0
+             , size = annotation_sz, label = 'Arch. Matured') +
+    annotate('text', x = 21, y = 252.4 + 10, hjust = 0
+             , size = annotation_sz, label = 'Full Functional') +
+    annotate('text', x = 26, y = 101.4 + 10, hjust = 0
+             , size = annotation_sz, label = 'Feature Rich') +
+    labs(title = 'Efforts Burndown'
+         , subtitle = 'Planned vs Actual') +
     my_theme()
 
 svg('burndown.svg', width = 8, height = 5)
--- features-dumbbell.R
+++ features-dumbbell.R
@@ -1,10 +1,9 @@
 library(ggplot2); library(ggalt)
 
 my_theme <- function() {
-    theme_bw(base_size = 12, base_family = 'SF Pro Display') +
-        theme(plot.title = element_text(vjust = 1.25, face = 'bold'),
-              axis.ticks = element_blank(),
-              plot.margin = unit(c(1, 0.5, .75, .5), "cm"))
+    theme_minimal(base_size = 12, base_family = 'SF Pro Display') +
+        theme(plot.title = element_text(vjust = 1.25, face = 'bold')
+              )
 }
 
 dat <- read.csv('feature-list.csv')
@@ -14,14 +13,15 @@ dat$Feature <- factor(dat$Feature, levels = as.character(dat$Feature))
 
 max_efforts <- max(dat$Efforts)
 
-plot <- ggplot(dat, aes(x = Remaining, xend = Efforts, y = Feature,
-                        group = Efforts)) +
+plot <- ggplot(dat, aes(x = Remaining, xend = Efforts, y = Feature
+                        , group = Efforts)) +
     geom_dumbbell(colour = '#a3c4dc', colour_xend = '#0e668b', size_xend = 3) +
-    scale_x_continuous(limits = c(0, max_efforts),
-        breaks = seq(0, max_efforts, by = 20), name = 'effort (points)') +
-    labs(y = NULL,
-         title = 'Feature Efforts',
-         subtitle = 'Day 0 vs Remainning') +
+        scale_x_continuous(limits = c(0, max_efforts)
+                             , breaks = seq(0, max_efforts, by = 20)
+                             , name = 'effort (points)') +
+        labs(y = NULL
+               , title = 'Feature Efforts'
+               , subtitle = 'Day 0 vs Remainning') +
     my_theme()
 
 svg('features-dumbbell.svg', width = 7, height = 6)
