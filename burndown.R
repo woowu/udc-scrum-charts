d0f5b6f7fcd559965866a27a279fd6497c993474
Woody Wu <woody.wu2@honeywell.com>
2019-08-25 16:38:31 +0800

    auto annotate milestones

--- burndown.R
+++ burndown.R
@@ -1,15 +1,17 @@
-library(ggplot2);
+library(ggplot2); library(purrr); library(RColorBrewer);
 
-ystep <- 50
+ystep <- 100
 guide_line_color <- '#c0c0c0'
 guide_line_sz <- .2
 curr_sprint <- 11
-annotation_sz <- 3
+annotation_point_sz <- 3
+lowlight_color <- brewer.pal(n = 8, name = 'Dark2')[8]
+highlight_color <- brewer.pal(n = 8, name = 'Dark2')[4]
+font <- 'Roboto'
+font_sz <- 16
 
-my_theme <- function() {
-    theme_minimal(base_size=12, base_family = 'SF Pro Display') +
-        theme(legend.title = element_blank()) +
-        theme(plot.title = element_text(vjust = 1.25, face = 'bold'))
+mid <- function(min, max, dist) {
+    ceiling(min + (max - min) * dist)
 }
 
 dat <- read.csv('burndown.csv')
@@ -20,42 +22,53 @@ head <- c('sprint', 'efforts', 'type')
 names(baseline) <- head
 names(actual) <- head
 burndown <- rbind(baseline, actual)
+baseline <- subset(burndown, burndown$type == 'baseline')
 
 max_efforts <- max(burndown$efforts)
 min_sprint <- min(burndown$sprint)
 max_sprint <- max(burndown$sprint)
+sprints_num <- nrow(baseline)
 velocity <- max_efforts / (max_sprint - min_sprint)
 
-min(actual$efforts)
+key_sprints <- baseline[c(mid(1, sprints_num, .2)
+                          , mid(1, sprints_num, .4)
+                          , mid(1, sprints_num, .6)
+                          , mid(1, sprints_num, .8)),]
+key_sprints$name <- c('MVP'
+                      , 'Arch. Mature'
+                      , 'Full Functional'
+                      , 'Feature Rich');
+sprint_colors <- rep(lowlight_color, sprints_num)
+sprint_colors[curr_sprint - min_sprint + 1] <- highlight_color
+
+my_theme <- function() {
+    theme_minimal(base_size = font_sz, base_family = font) +
+        theme(legend.title = element_blank()) +
+        theme(plot.title = element_text(vjust = 1.25, face = 'bold')) +
+        theme(axis.text.x = element_text(color = sprint_colors)) +
+        theme(axis.text.y = element_text(color = lowlight_color))
+}
+
 plot <- ggplot(burndown, aes(sprint, efforts)) +
     geom_line(aes(color = type, size = type != 'baseline')) +
+    scale_color_brewer(palette = 'Dark2') +
     scale_x_continuous(limits = c(min_sprint, max_sprint),
         breaks = seq(min_sprint, max_sprint, by=1), name = 'sprint (2w)') +
     scale_y_continuous(limits = c(0, max_efforts)
         , breaks = seq(0, max_efforts, by=ystep)
         , name = 'remaining effort (points)') +
     scale_size_manual(values = c(0.5, 1.2), guide = FALSE) +
-    geom_segment(aes(x = curr_sprint, xend = curr_sprint
-                     , y = 0, yend = 554.40 - 10)
-                    , color = guide_line_color, size = guide_line_sz) +
-    geom_segment(aes(x = 17, xend = 17, y = 0, yend = 373.2 - 10),
-                 color = guide_line_color, size = guide_line_sz) +
-    geom_segment(aes(x = 21, xend = 21, y = 0, yend = 252.4 - 10),
-                 color = guide_line_color, size = guide_line_sz) +
-    geom_segment(aes(x = 26, xend = 26, y = 0, yend = 101.4 - 10),
-                 color = guide_line_color, size = guide_line_sz) +
-    annotate('text', x = curr_sprint, y = 554.40 + 10, hjust = 0
-             , size = annotation_sz, label = 'Today') +
-    annotate('text', x = 17, y = 373.2 + 10, hjust = 0
-             , size = annotation_sz, label = 'Arch. Matured') +
-    annotate('text', x = 21, y = 252.4 + 10, hjust = 0
-             , size = annotation_sz, label = 'Full Functional') +
-    annotate('text', x = 26, y = 101.4 + 10, hjust = 0
-             , size = annotation_sz, label = 'Feature Rich') +
-    labs(title = 'Efforts Burndown'
+    geom_point(data = key_sprints, aes(sprint, efforts), shape = 1
+               , size = annotation_point_sz
+               , color = lowlight_color) +
+    geom_text(data = key_sprints, aes(sprint, efforts, label = name)
+              , color = lowlight_color
+              , hjust = -.1, vjust = -1) +
+    labs(title = 'Efforts Burndown w/ Release Milestones'
          , subtitle = 'Planned vs Actual') +
     my_theme()
 
-svg('burndown.svg', width = 8, height = 5)
-plot
-dev.off()
+ggsave(file = 'burndown.svg', width = 8, height = 5, units = 'in')
+ggsave(file = 'burndown.png', width = 8, height = 5, units = 'in'
+       , dpi = 96)
+
--- features-dumbbell.R
+++ features-dumbbell.R
@@ -1,7 +1,12 @@
-library(ggplot2); library(ggalt)
+library(ggplot2); library(ggalt); library(RColorBrewer);
+
+font <- 'Roboto'
+font_sz <- 16
+#palette <- brewer.pal(n = 11, name = 'RdBu')
+palette <- brewer.pal(n = 11, name = 'Dark2')
 
 my_theme <- function() {
-    theme_minimal(base_size = 12, base_family = 'SF Pro Display') +
+    theme_minimal(base_size = font_sz, base_family = font) +
         theme(plot.title = element_text(vjust = 1.25, face = 'bold')
               )
 }
@@ -15,7 +20,9 @@ max_efforts <- max(dat$Efforts)
 
 plot <- ggplot(dat, aes(x = Remaining, xend = Efforts, y = Feature
                         , group = Efforts)) +
-    geom_dumbbell(colour = '#a3c4dc', colour_xend = '#0e668b', size_xend = 3) +
+    geom_dumbbell(colour = palette[1]
+                  , colour_xend = palette[2]
+                  , size_xend = 3) +
         scale_x_continuous(limits = c(0, max_efforts)
                              , breaks = seq(0, max_efforts, by = 20)
                              , name = 'effort (points)') +
@@ -24,6 +31,7 @@ plot <- ggplot(dat, aes(x = Remaining, xend = Efforts, y = Feature
                , subtitle = 'Day 0 vs Remainning') +
     my_theme()
 
-svg('features-dumbbell.svg', width = 7, height = 6)
-plot
-dev.off()
+ggsave(file = 'features-dumbbell.svg', width = 8, height = 5, units = 'in')
+ggsave(file = 'features-dumbbell.png', width = 8, height = 5, units = 'in'
+       , dpi = 96)
+
