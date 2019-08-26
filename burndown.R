c357f4a5129487c796bc1b3725365980ce2857ad
Woody Wu <woody.wu2@honeywell.com>
2019-08-26 11:24:22 +0800

    baseline -> forecast

--- burndown.R
+++ burndown.R
@@ -5,8 +5,9 @@ guide_line_color <- '#c0c0c0'
 guide_line_sz <- .2
 curr_sprint <- 11
 annotation_point_sz <- 3
-lowlight_color <- brewer.pal(n = 8, name = 'Dark2')[8]
+tick_label_color <- brewer.pal(n = 8, name = 'Dark2')[8]
 highlight_color <- brewer.pal(n = 8, name = 'Dark2')[4]
+annotation_color <- brewer.pal(n = 8, name = 'Dark2')[4]
 font <- 'Roboto'
 font_sz <- 16
 
@@ -15,22 +16,22 @@ mid <- function(min, max, dist) {
 }
 
 dat <- read.csv('burndown.csv')
-baseline <- data.frame(dat$Sprint, dat$Baseline, rep('baseline', dim(dat)[1]))
-actual <- data.frame(dat$Sprint, dat$Actual, rep('actual', dim(dat)[1]))
-actual <- subset(actual, ! is.na(dat.Actual))
+forecast <- data.frame(dat$sprint, dat$forecast, rep('forecast', dim(dat)[1]))
+actual <- data.frame(dat$sprint, dat$actual, rep('actual', dim(dat)[1]))
+actual <- subset(actual, ! is.na(dat.actual))
 head <- c('sprint', 'efforts', 'type')
-names(baseline) <- head
+names(forecast) <- head
 names(actual) <- head
-burndown <- rbind(baseline, actual)
-baseline <- subset(burndown, burndown$type == 'baseline')
+burndown <- rbind(forecast, actual)
+forecast <- subset(burndown, burndown$type == 'forecast')
 
 max_efforts <- max(burndown$efforts)
 min_sprint <- min(burndown$sprint)
 max_sprint <- max(burndown$sprint)
-sprints_num <- nrow(baseline)
+sprints_num <- nrow(forecast)
 velocity <- max_efforts / (max_sprint - min_sprint)
 
-key_sprints <- baseline[c(mid(1, sprints_num, .2)
+key_sprints <- forecast[c(mid(1, sprints_num, .2)
                           , mid(1, sprints_num, .4)
                           , mid(1, sprints_num, .6)
                           , mid(1, sprints_num, .8)),]
@@ -38,7 +39,7 @@ key_sprints$name <- c('MVP'
                       , 'Arch. Mature'
                       , 'Full Functional'
                       , 'Feature Rich');
-sprint_colors <- rep(lowlight_color, sprints_num)
+sprint_colors <- rep(tick_label_color, sprints_num)
 sprint_colors[curr_sprint - min_sprint + 1] <- highlight_color
 
 my_theme <- function() {
@@ -46,11 +47,11 @@ my_theme <- function() {
         theme(legend.title = element_blank()) +
         theme(plot.title = element_text(vjust = 1.25, face = 'bold')) +
         theme(axis.text.x = element_text(color = sprint_colors)) +
-        theme(axis.text.y = element_text(color = lowlight_color))
+        theme(axis.text.y = element_text(color = tick_label_color))
 }
 
 plot <- ggplot(burndown, aes(sprint, efforts)) +
-    geom_line(aes(color = type, size = type != 'baseline')) +
+    geom_line(aes(color = type, size = type != 'forecast')) +
     scale_color_brewer(palette = 'Dark2') +
     scale_x_continuous(limits = c(min_sprint, max_sprint),
         breaks = seq(min_sprint, max_sprint, by=1), name = 'sprint (2w)') +
@@ -60,15 +61,15 @@ plot <- ggplot(burndown, aes(sprint, efforts)) +
     scale_size_manual(values = c(0.5, 1.2), guide = FALSE) +
     geom_point(data = key_sprints, aes(sprint, efforts), shape = 1
                , size = annotation_point_sz
-               , color = lowlight_color) +
+               , color = tick_label_color) +
     geom_text(data = key_sprints, aes(sprint, efforts, label = name)
-              , color = lowlight_color
+              , color = annotation_color
               , hjust = -.1, vjust = -1) +
     labs(title = 'Efforts Burndown w/ Release Milestones'
-         , subtitle = 'Planned vs Actual') +
+         , subtitle = 'Forecast vs Actual') +
     my_theme()
 
-ggsave(file = 'burndown.svg', width = 8, height = 5, units = 'in')
-ggsave(file = 'burndown.png', width = 8, height = 5, units = 'in'
-       , dpi = 96)
+ggsave(file = 'burndown.svg', width = 8, height = 6, units = 'in')
+ggsave(file = 'burndown.png', width = 8, height = 6, units = 'in'
+       , dpi = 300)
 
--- features-dumbbell.R
+++ features-dumbbell.R
@@ -2,8 +2,7 @@ library(ggplot2); library(ggalt); library(RColorBrewer);
 
 font <- 'Roboto'
 font_sz <- 16
-#palette <- brewer.pal(n = 11, name = 'RdBu')
-palette <- brewer.pal(n = 11, name = 'Dark2')
+palette <- brewer.pal(n = 8, name = 'Dark2')
 
 my_theme <- function() {
     theme_minimal(base_size = font_sz, base_family = font) +
@@ -12,14 +11,14 @@ my_theme <- function() {
 }
 
 dat <- read.csv('feature-list.csv')
-dat$Remaining <- dat$Efforts * (1 - dat$Completed)
-dat <- dat[order(dat$Efforts),]
-dat$Feature <- factor(dat$Feature, levels = as.character(dat$Feature))
+dat$remaining <- dat$efforts * (1 - dat$completed)
+dat <- dat[order(dat$efforts),]
+dat$feature <- factor(dat$feature, levels = as.character(dat$feature))
 
-max_efforts <- max(dat$Efforts)
+max_efforts <- max(dat$efforts)
 
-plot <- ggplot(dat, aes(x = Remaining, xend = Efforts, y = Feature
-                        , group = Efforts)) +
+plot <- ggplot(dat, aes(x = remaining, xend = efforts, y = feature
+                        , group = efforts)) +
     geom_dumbbell(colour = palette[1]
                   , colour_xend = palette[2]
                   , size_xend = 3) +
@@ -27,11 +26,11 @@ plot <- ggplot(dat, aes(x = Remaining, xend = Efforts, y = Feature
                              , breaks = seq(0, max_efforts, by = 20)
                              , name = 'effort (points)') +
         labs(y = NULL
-               , title = 'Feature Efforts'
+               , title = 'PBI Estimation'
                , subtitle = 'Day 0 vs Remainning') +
     my_theme()
 
-ggsave(file = 'features-dumbbell.svg', width = 8, height = 5, units = 'in')
-ggsave(file = 'features-dumbbell.png', width = 8, height = 5, units = 'in'
-       , dpi = 96)
+ggsave(file = 'features-dumbbell.svg', width = 8, height = 6, units = 'in')
+ggsave(file = 'features-dumbbell.png', width = 8, height = 6, units = 'in'
+       , dpi = 300)
 
